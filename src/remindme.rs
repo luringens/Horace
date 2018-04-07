6508a2ba86783ddf0d02895602b40e9f54412a60
Stian Soltvedt <soltvedt.stian@gmail.com>
2018-04-07 19:26:13 +0200

    Initial sketch of !remindme implementation

--- src/remindme.rs
+++ src/remindme.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use serenity::model::channel::Message;
 use chrono::Duration;
 
@@ -5,7 +7,32 @@ use command_error::*;
 
 static a: &str = "Usage:";
 
+/// Ideas for self:
+/// Store a list of remind dates in a table in the DB
+/// Have a seperate thread check the table once every hour or so,
+/// and perform and delete any expired reminders.
+
 pub fn remindme(msg: &Message) -> Result<String, CommandError> {
+    let parts: Vec<&str> = msg.content.split_whitespace()
+        .skip(1)
+        .collect();
+    
+    if parts.len() != 2 {
+        return Err(CommandError::Generic(a.to_owned()))
+    }
+
+    let num = match i32::from_str(parts[0]) {
+        Ok(i) => i,
+        Err(_) => return Err(CommandError::Generic(a.to_owned())),
+    };
+
+    match &*(parts[1].to_lowercase()) {
+        "days" => {
+            
+        }
+        _ => return Err(CommandError::Generic(a.to_owned())),
+    }
+
     unimplemented!()
 }
 
